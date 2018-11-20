083e59b137d5f59bdc836b65ddf288f31e2fa67f
May Yassin <mayyassin@Mays-MacBook-Pro.local>
2018-11-20 17:10:17 +0300

    api half done

--- api/models.py
+++ api/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Product(models.Model):
+	name = models.CharField(max_length=120)
+	description = models.TextField()
+	price = models.DecimalField(max_digits=10, decimal_places=3)
+	img = models.ImageField()
+
+	def __str__(self):
+		return self.name
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, default=1, on_delete=models.CASCADE)
+	dob = models.DateField(null=True, blank=True)
+	profile_pic = models.ImageField(default="/profile_pic/pic placeholder.png/",upload_to='profile_pic', null=True, blank=True)
+	
+	def __str__(self):
+		return "ID:%s User:%s " % (self.id, self.user.username)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
--- api/views.py
+++ api/views.py
@@ -1,5 +1,45 @@
-from rest_framework.generics import CreateAPIView
-from .serializers import UserCreateSerializer
+
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+    CreateAPIView,
+)
+from .serializers import (
+    ProductListSerializer,
+    ProductDetailSerializer,
+    UserCreateSerializer,
+    ProfileSerializer,
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import *
+from .permissions import *
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+    permission_classes = [AllowAny,]
+    filter_backends = [SearchFilter, OrderingFilter,]
+    search_fields = ['name', 'description',]
+
+
+
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'product_id'
+    permission_classes = [AllowAny,]
+
+
+class ProfileView(RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'user_id'
+    permission_classes = [IsUser, ]
