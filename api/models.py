70b60a4d2416b9108a3257348aefecc744cd2b7e
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-11-26 15:06:11 +0300

    fix product field

--- api/models.py
+++ api/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
 	price = models.DecimalField(max_digits=10, decimal_places=3)
 	img = models.ImageField()
 	quantity = models.PositiveIntegerField(default=0)
-	category = models.CharField(max_length=1, default=0, choices=CATEGORY_CHOICE)
+	category = models.CharField(max_length=20, default=0, choices=CATEGORY_CHOICE)
 
 	def __str__(self):
 		return self.name
